1b119db4802e87392a9d5b5640e67021b7b950bf
Yali Izzo <Sciman1011@Gmail.com>
2021-01-21 11:08:57 -0500

    Trying to debug motor issues

--- app.py
+++ app.py
@@ -4,33 +4,33 @@ from motor import Motor
 app = Flask(__name__)
 
 motorLeft = Motor(35,23,24)
-motorRight = Motor(17,22,27)
+#motorRight = Motor(32,21,22)
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         if request.form.get('Forward') == 'Forward':
-            motorRight.write(100)
+            #motorRight.write(100)
             motorLeft.write(100)
             print("Motor Forward")
 
         elif request.form.get('Stop') == 'Stop':
-            motorRight.write(0)
-            motorLeft.write(0)
+            #motorRight.write(0)
+            motorLeft.stop()
             print("Motor Stop")
 
         elif request.form.get('Backwards') == 'Backwards':
-            motorRight.write(-100)
+            #motorRight.write(-100)
             motorLeft.write(-100)
             print("Motor Back")
 
         elif request.form.get("Left")==("Left"):
-            motorRight.write(100)
+            #motorRight.write(100)
             motorLeft.write(-100)
             print("Motor Left")
 
         elif request.form.get("Right")==("Right"):
-            motorRight.write(-100)
+            #motorRight.write(-100)
             motorLeft.write(100)
             print("Motor Right")
 
