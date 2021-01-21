e2c39d16b3a820921473699deef292a316dcbfd1
Yali Izzo <Sciman1011@Gmail.com>
2021-01-21 10:37:49 -0500

    Changed app host

--- webInterface.py
+++ webInterface.py
@@ -28,5 +28,5 @@ def index():
               return render_template("index.html")
                                                       
 if __name__ == '__main__':
-              app.run(host='172.16.0.110')
+              app.run()
                   
