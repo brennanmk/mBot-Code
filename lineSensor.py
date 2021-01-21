f12a5fe40aa63053b73f0df77f60a38689d7b006
Brennan <brennanmk2200@gmail.com>
2021-01-20 19:47:26 -0500

    Cleaned up libs

--- lineSensor.py
+++ lineSensor.py
@@ -1,10 +1,10 @@
 import RPi.GPIO as GPIO
 
+in=6 #Input pin for the yellow wire on the linefinder is GPIO pin 6
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(in, GPIO.IN)
+
 def readSensor():
-    in=6 #Input pin for the yellow wire on the linefinder is GPIO pin 6
-
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(in, GPIO.IN)
-
     return GPIO.input(in)
 
--- motorControl.py
+++ motorControl.py
@@ -1,36 +1,36 @@
 import RPi.GPIO as GPIO          
 from time import sleep
 
+#in Declarations for Motor1
+in1 = 24
+in2 = 23
+en1 = 25
+
+#Pin Declarations for Motor2
+in3=22
+in4=27
+en2=17
+
+#Motor1 Setup
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(in1,GPIO.OUT)
+GPIO.setup(in2,GPIO.OUT)
+GPIO.setup(en1,GPIO.OUT)
+GPIO.output(in1,GPIO.LOW)
+GPIO.output(in2,GPIO.LOW)
+p=GPIO.PWM(en1,1000)
+p.start(25)
+
+#Motor2 Setup
+GPIO.setup(in3,GPIO.OUT)
+GPIO.setup(in4,GPIO.OUT)
+GPIO.setup(en2,GPIO.OUT)
+GPIO.output(in3,GPIO.LOW)
+GPIO.output(in4,GPIO.LOW)
+p=GPIO.PWM(en2,1000)
+p.start(25)
+
 def motorControl(value):
-    #Pin Declarations for Motor1
-    in1 = 24
-    in2 = 23
-    en1 = 25
-
-    #Pin Declarations for Motor2
-    in3=22
-    in4=27
-    en2=17
-
-    #Motor1 Setup
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(in1,GPIO.OUT)
-    GPIO.setup(in2,GPIO.OUT)
-    GPIO.setup(en1,GPIO.OUT)
-    GPIO.output(in1,GPIO.LOW)
-    GPIO.output(in2,GPIO.LOW)
-    p=GPIO.PWM(en1,1000)
-    p.start(25)
-
-    #Motor2 Setup
-    GPIO.setup(in3,GPIO.OUT)
-    GPIO.setup(in4,GPIO.OUT)
-    GPIO.setup(en2,GPIO.OUT)
-    GPIO.output(in3,GPIO.LOW)
-    GPIO.output(in4,GPIO.LOW)
-    p=GPIO.PWM(en2,1000)
-    p.start(25)
-
     #Control the motor based on the value entered (0 stop, 1 forward, 2 backward, 3 left, 4 right)
     if value==1:
         GPIO.output(in1,GPIO.HIGH)
