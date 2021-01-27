3a559ca33f5e417871c1f5df4d97c2f33a7d10c9
Yali Izzo <Sciman1011@Gmail.com>
2021-01-27 15:32:35 -0500

    Added time blocks

--- static/js/robot_blocks.js
+++ static/js/robot_blocks.js
@@ -37,6 +37,21 @@ Blockly.Blocks['robot_get_line'] = {
    this.setHelpUrl("");
     }
   };
+  Blockly.Blocks['time_sleep'] = {
+	init: function() {
+	  this.appendValueInput("TIME")
+		  .setCheck("Number")
+		  .appendField("Wait for");
+	  this.appendDummyInput()
+		  .appendField("seconds");
+	  this.setInputsInline(true);
+	  this.setPreviousStatement(true, null);
+	  this.setNextStatement(true, null);
+	  this.setColour(230);
+   this.setTooltip("");
+   this.setHelpUrl("");
+	}
+  };
 
 // Define block functionality
 Blockly.Python['robot_get_line'] = function(block) {
@@ -52,5 +67,10 @@ Blockly.Python['robot_get_line'] = function(block) {
     const dropdown_motor = block.getFieldValue('MOTOR');
     let value_power = Blockly.Python.valueToCode(block, 'POWER', Blockly.Python.ORDER_ATOMIC);
     const code = 'robot.setMotorPower(' + '\"'+ dropdown_motor +'\",' + value_power + ')\n';
-    return code;
+    return [code, Blockly.Python.ORDER_NONE];
+  };
+
+  Blockly.Python['time_sleep'] = function(block) {
+	const value_time = Blockly.Python.valueToCode(block, 'TIME', Blockly.Python.ORDER_ATOMIC);
+	return 'time.sleep('+value_time+')\n';
   };
