24fe0aa7b05cf8dd129e277a2fed6eb5c0e930f0
Yali Izzo <Sciman1011@Gmail.com>
2021-01-27 19:12:23 -0500

    Added additional blocks

--- static/js/robot_blocks.js
+++ static/js/robot_blocks.js
@@ -52,6 +52,43 @@ Blockly.Blocks['robot_get_line'] = {
    this.setHelpUrl("");
 	}
   };
+  Blockly.Blocks['robot_drive'] = {
+	init: function() {
+	  this.appendDummyInput()
+		  .appendField("Drive")
+		  .appendField(new Blockly.FieldDropdown([["forwards","FWD"], ["backwards","BCK"]]), "DIRECTION");
+	  this.appendValueInput("DURATION")
+		  .setCheck("Number")
+		  .appendField("for");
+	  this.appendDummyInput()
+		  .appendField("seconds");
+	  this.setInputsInline(true);
+	  this.setPreviousStatement(true, null);
+	  this.setNextStatement(true, null);
+	  this.setColour(330);
+   this.setTooltip("");
+   this.setHelpUrl("");
+	}
+  };
+  
+  Blockly.Blocks['robot_turn'] = {
+	init: function() {
+	  this.appendDummyInput()
+		  .appendField("Turn")
+		  .appendField(new Blockly.FieldDropdown([["right","RIGHT"], ["left","LEFT"]]), "DIRECTION");
+	  this.appendValueInput("DURATION")
+		  .setCheck("Number")
+		  .appendField("for");
+	  this.appendDummyInput()
+		  .appendField("seconds");
+	  this.setInputsInline(true);
+	  this.setPreviousStatement(true, null);
+	  this.setNextStatement(true, null);
+	  this.setColour(330);
+   this.setTooltip("");
+   this.setHelpUrl("");
+	}
+  };
 
 // Define block functionality
 Blockly.Python['robot_get_line'] = function(block) {
@@ -73,4 +110,23 @@ Blockly.Python['robot_get_line'] = function(block) {
   Blockly.Python['time_sleep'] = function(block) {
 	const value_time = Blockly.Python.valueToCode(block, 'TIME', Blockly.Python.ORDER_ATOMIC);
 	return 'time.sleep('+value_time+')\n';
+  };
+  Blockly.Python['robot_drive'] = function(block) {
+	const dropdown_direction = block.getFieldValue('DIRECTION');
+	const value_duration = Blockly.Python.valueToCode(block, 'DURATION', Blockly.Python.ORDER_ATOMIC);
+	const spd = (dropdown_direction == 'FWD' ? 100 : -100);
+	let code = 'robot.setMotorPower("left",' + spd + ')\n';
+	code += 'robot.setMotorPower("right",' + spd + ')\n';
+	code += 'time.sleep(' + value_duration + ')\n'
+	return code;
+  };
+  
+  Blockly.Python['robot_turn'] = function(block) {
+	const dropdown_direction = block.getFieldValue('DIRECTION');
+	const value_duration = Blockly.Python.valueToCode(block, 'DURATION', Blockly.Python.ORDER_ATOMIC);
+	const spd = (dropdown_direction == 'RIGHT' ? 100 : -100);
+	let code = 'robot.setMotorPower("left",' + (-spd) + ')\n';
+	code += 'robot.setMotorPower("right",' + spd + ')\n';
+	code += 'time.sleep(' + value_duration + ')\n'
+	return code;
   };
