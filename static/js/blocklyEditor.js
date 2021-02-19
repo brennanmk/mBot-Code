b6f0f5735b990d677cc24747d293ff1300a98980
Yali Izzo <Sciman1011@Gmail.com>
2021-02-19 14:04:43 -0500

    Added import/export/clear buttons

--- static/js/blocklyEditor.js
+++ static/js/blocklyEditor.js
@@ -34,7 +34,7 @@ function blocklyUpdateEvent(e) {
 	if (saveTimeout != null) {
 		clearTimeout(saveTimeout);
 	}
-	saveTimeout = setTimeout(saveWorkspace,1000);
+	saveTimeout = setTimeout(saveWorkspace, 1000);
 	saveMessage.innerText = 'Saving...';
 }
 workspace.addChangeListener(blocklyUpdateEvent);
@@ -42,32 +42,85 @@ workspace.addChangeListener(blocklyUpdateEvent);
 
 // Save the workspace to local storage
 function saveWorkspace() {
-	if (typeof(Storage) !== "undefined") {
+	if (typeof (Storage) !== "undefined") {
 		const xml = Blockly.Xml.workspaceToDom(Blockly.mainWorkspace);
-		localStorage.setItem(SAVE_NAME,Blockly.Xml.domToText(xml));
+		localStorage.setItem(SAVE_NAME, Blockly.Xml.domToText(xml));
 
 		saveMessage.innerText = 'Saved';
-	}else{
+	} else {
 		console.log("Error saving workspace");
 	}
 }
 
 function loadWorkspace() {
-	if (typeof(Storage) !== "undefined") {
+	if (typeof (Storage) !== "undefined") {
 		if (localStorage.hasOwnProperty(SAVE_NAME)) {
 			const xml = Blockly.Xml.textToDom(localStorage.getItem(SAVE_NAME));
-			Blockly.Xml.domToWorkspace(Blockly.mainWorkspace,xml);
+			Blockly.Xml.domToWorkspace(Blockly.mainWorkspace, xml);
 			console.log("Loaded saved workspace!");
 
 			blocklyUpdateEvent(null);
-		}else{
+		} else {
 			console.log("LocalStorage is null");
 		}
-	}else{
+	} else {
 		console.log("Error loading workspace");
 	}
 }
 
+
+// Clear workspace
+document.getElementById('tool-clear').onclick = function (e) {
+	if (confirm("Clear workspace?")) {
+		Blockly.mainWorkspace.clear();
+	}
+}
+
+// Download workspace as file
+document.getElementById('tool-export').onclick = function (e) {
+	const xml = Blockly.Xml.workspaceToDom(Blockly.mainWorkspace);
+
+	// https://stackoverflow.com/questions/45831191/generate-and-download-file-from-js
+	// Save the current code as a text file
+	const element = document.createElement('a');
+	element.href = 'data:text/plain;charset=utf-8,' + encodeURI(Blockly.Xml.domToText(xml));
+	element.download = 'Robot Code.txt';
+	element.target = '_blank';
+
+	element.click();
+}
+
+// Upload workspace as file
+document.getElementById('tool-import').onclick = function (e) {
+
+	const input = document.createElement('input');
+	input.type = 'file';
+
+	input.onchange = e => {
+
+		const file = e.target.files[0];
+
+		const reader = new FileReader();
+		reader.readAsText(file, 'UTF-8');
+
+		// When the reader loads successfully...
+		reader.onload = re => {
+			try {
+				const xml = Blockly.Xml.textToDom(re.target.result);
+				Blockly.Xml.domToWorkspace(Blockly.mainWorkspace, xml);
+				console.log("Loaded saved workspace!");
+
+				blocklyUpdateEvent(null);
+			}catch (err) {
+				alert("Error importing workspace!");
+			}
+		}
+
+	};
+
+	input.click();
+
+}
 
 // Send code on button press
 document.getElementById('run-code').onclick = function (e) {
